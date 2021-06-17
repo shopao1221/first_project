7771d16b12cdf64d48b05e5984bfad6533b4c6b1
Paulo pogi <shopao1221@gmail.com>
2021-06-17 09:12:03 +0800

    FIRST COMMIT

---
+++ index.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+        <!------------------------ NavigationBar --------------------------->
+        
+        <!-- <div class="d-none  d-lg-block"> -->
+<div class="container-fluid top-nav">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- <a class="navbar-brand" href="#"><img src="image/logo.png" alt="" style="height: 100%;
+    width: 150px;"></a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav nav-menus">
+      <li class="nav-item">
+        <a class="nav-link" href="#">STUDY DESIGN</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"  data-toggle="modal" data-target="#exampleModal">REFERENCES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">IMPORTANT SAFETY IMPORMATION</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+
+
+
+</div>
+<!-- </div> -->
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl" style="padding-top:150px;">
+    <div class="modal-content" style="border-radius:2rem;">
+      <div class="modal-header mod-head">
+        <h5 class="modal-title mod-title" id="exampleModalLabel">REFERENCES</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true" style="color:white;">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <ul style="list-style-type:none;padding: 0 0 0 20px; margin: 0;">
+          <li class="p-3">
+              <p class="mb-0" style="color: #435464;">1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati enim reiciendis optio ex commodi consectetur molestias libero quae sed officiis amet voluptates quis itaque culpa necessitatibus, eius aspernatur, doloribus tempora.</p>
+          </li>
+          <li class="p-3">
+          <p class="mb-0" style="color: #435464;">2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati enim reiciendis optio ex commodi consectetur molestias libero quae sed officiis amet voluptates quis itaque culpa necessitatibus, eius aspernatur, doloribus tempora.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+        <!-- mobile view -->
+        <!-- <div class="d-sm-block d-md-none">   
+    <div class="container-fluid top-nav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-toggle="modal" data-target="#exampleModal1" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        </nav>
+    </div>
+</div> -->
+
+
+    <!-- Modal -->
+<!-- <div class="modal left fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header pb-0">
+        <h5 class="modal-title" id="exampleModalLabel">Menus</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <hr class="mt-0">
+
+            
+            <ul style="list-style-type:none;padding: 0 0 0 20px; margin: 0;">
+                <li class="p-3">
+                    <a class="a-color" href="#"><i class="fas fa-star-of-life"></i><span style="margin-left: .5rem;">STUDY DESIGN</span> </a>
+                </li>
+                <li class="p-3">
+                <a class="a-color" href="#"><i class="fab fa-sourcetree"></i><span style="margin-left: .5rem;">references</span> </a>
+                </li>
+                <li class="p-3 ml-1">
+                <a class="a-color" href="#"><i class="fas fa-info"></i><span style="margin-left: .5rem;">important safety impormation</span> </a>
+                </li>
+            </ul>
+            <hr class="mt-0">
+      </div>
+    </div>
+  </div>
+</div> -->
+
+
+
+
+
+        
+
+<!------------------------ Header --------------------------->
+<div class="container">
+
+<div class="jumbotron p-0" style="background-color: transparent;">
+<div class="row">
+    
+    <div class="col-sm-12 col-md-7 col-lg-7" style="padding-top:5rem;">
+    <h1 class="display-4 logo"><img src="image/logo.png" alt="" style="height:100px;"></h1>
+      <p class="lead mb-0"><strong style="color:green;">XEKIZUMAB (TALITZ)  </strong>  - A TARGET IL-17 A INHIBITOR WITH HIGH BINDING FAMILY</p>
+        <p class="com">COMPLETELY <br> CLEAR SKIN</p>
+      <!-- <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p> -->
+    </div>
+    
+    <div class="col-sm-6 col-md-5 offset-md-2 col-lg-5 offset-lg-0">
+    <img class="hands" src="image/hand.png" alt="">
+    </div>
+
+    <div class="col-sm-12">
+    <p class="mb-0 pt-0 desk">IXEKIZUMAB (TALITZ) is the indicated for a treatment of moderate to severe plaque psoriasis in adults who are candidates for systematic theraphy .2 IXEKIZUMAB (TALITZ), alone or in combination with methotrexate, is indicated for the treatment of active psoriasis arthritisin adult patiets who have responded inadiquetly to, or who are intolerant to one or more disease modifying anti-rhuematic drug (DMARD) therapist (see section 6.1)2.</p>   
+    </div>  
+
+  </div>
+</div>
+</div>
+
+
+<div class="container-fluid mb-5">
+  <div class="row">
+    <div class="col-sm-6" ><p class="inter">IL-interluekin</p></div>
+    <div class="col-sm-6" style="padding-left: 30rem;"><img src="image/lilys.png" alt="" style="width:50%;"> <br> <p style="font-size: 1.1rem;font-weight: 600;color: #435464;">PH-ELI-TAL-OTH-HCP-0576-V1</p></div>
+  </div>
+</div>
+
+
+
+
+
+<!------------------------ Patients expectaions --------------------------->
+
+<div class="container-fluid pr-0 pl-0">
+
+<div class="card" style="border:none;">
+  <div class="card-body">
+  <div class="row">
+    <div class="col-7">
+      <p class="mb-0 pl-5" style="color: #B1BCC0;font-siza: 1.2rem;font-weight: 600;">BURDEN OF DISEASE: <span style="color: #465364;">Patient Expectations</span></p>
+      <p class="pl-5" style="color: #4BAB47;font-size: xx-large;">Who could be candidate for Ixekizumab (Taltz)?</p>
+    </div>
+    <div class="col-5">
+    <img src="image/logo.png" class="rounded float-left" alt="..." style="height:100px; padding-left:20rem;">
+    </div>
+  </div>
+  </div>
+</div>
+</div>
+
+<div class="container mt-5">
+   
+        <div class="row">
+        <div class="section-title col-md-6" style="padding-top: 4rem;">
+            
+            <h1 class="titles">ELSA</h1>
+            <hr class="c-hr mt-0 mb-2" style="margin-left:2.4rem;">
+            <ul style="color: #49A945;"> 
+                <li><p style="font-size: larger;font-weight: 600;color: #315F6F;"> Moderate to severe-plaque psoriasis.</p></li>
+                <hr class="c-hrs">
+                <li><p style="font-size: larger;font-weight: 600;color: #315F6F;"> Dissatisfied with her level of skin clearance.</p></li>
+                <hr class="c-hrs"> 
+                <li><p style="font-size: larger;font-weight: 600;color: #315F6F;"> Plateaued on current treatment with residual plaques</p></li>
+                <hr class="c-hrs">
+                <li><p style="font-size: larger;font-weight: 600;color: #315F6F;"> Psoriasis is affecting her daily life activity.</p></li>
+                <hr class="c-hrs">
+                <li><p style="font-size: larger;font-weight: 600;color: #315F6F;">Uncomfortable about touching others or being  touched by others.</p></li>
+                <hr class="c-hrs">
+                <li><p style="font-size: larger;font-weight: 600;color: #315F6F;">Eager to see complete clearance that lasts, with rapidly visible result.</p></li>
+                <hr class="c-hrs">
+              </ul>
+          </div>
+          <div class="col-md-6">
+            <img src="image/elsa.jpg" alt="" style="height: 80%;width: 100%;margin-left: 7rem;">
+          </div>
+        </div>
+</div>
+
+<!------------------------ Psoriasis page --------------------------->
+
+<div class="container-fluid pr-0 pl-0">
+
+<div class="card" style="border:none;">
+  <div class="card-body">
+  <div class="row">
+    <div class="col-7">
+      <p class="mb-0 pl-5" style="color: #B1BCC0;font-siza: 1.2rem;font-weight: 600;">BURDEN OF DISEASE: <span style="color: #465364;">Patient Expectations</span></p>
+      <p class="pl-5" style="color: #4BAB47;font-size: xx-large;">Patient expectations from psoriasis treatment <br> (PsoBest registry)<span style="font-size:1rem;">1</span></p>
+    </div>
+    <div class="col-5">
+    <img src="image/logo.png" class="rounded float-left" alt="..." style="height:100px; padding-left:20rem;">
+    </div>
+  </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+<div class="container mt-5">
+   
+        <div class="row">
+              <div class="col-md-3">
+             
+                <div class="alert alert-primary" role="alert" style="background-color: #49AA41; border-radius: 2rem;">
+                    <button class="p-0" type="button" data-toggle="modal" data-target="#exampleModal1" style="border:none; position: absolute;background: #425560;height: 2rem;width: 2rem;top: .2rem;right: -0.5rem;border-radius: 100%;">
+                    <i class="fas fa-plus" style="color: white;font-size: 1rem;"></i>
+                      </button>
+                  <h1 style="padding-left: 4rem;font-weight: lighter;font-size: 4rem;color: white;">95%</h1>
+                  <h1 style="padding-left: .9rem;font-size: .8rem;color: white;margin-bottom:0;">of patients want to be  healed of</h1>
+                  <h1 style="padding-left: 2.8rem;color: white;margin-bottom:0;">all skin</h1>
+                  <h1 style="padding-left: 3rem;color: white;margin-bottom: 0;font-weight: 400;font-size: 2.2rem;">detects*</h1>
+                </div>
+              </div>
+              <div class="col">
+              <div class="alert alert-primary" role="alert" style="background-color: #164614; border-radius: 2rem;">
+                  <button class="p-0" type="button" data-toggle="modal" data-target="#exampleModal2" style="border:none; position: absolute;background: #425560;height: 2rem;width: 2rem;top: .2rem;right: -0.5rem;border-radius: 100%;">
+                        <i class="fas fa-plus" style="color: white;font-size: 1rem;"></i>
+                  </button>
+                  <h1 style="padding-left: 4rem;font-weight: lighter;font-size: 4rem;color: white;">95%</h1>
+                  <h1 style="padding-left: 4rem;font-size: .8rem;color: white;margin-bottom:0;">of patients want</h1>
+                  <h1 style="padding-left: .8rem;color: white;margin-bottom:0;">better skin</h1>
+                  <h1 style="padding-left: 3rem;color: white;margin-bottom: 0;font-weight: 400;font-size: 2.2rem;">quickly*</h1>
+                </div>
+              </div>
+          
+          <div class="col-md-6">
+            <img src="image/patient.jpg" alt="" style="height: 100%;width: 100%;margin-left: 7rem;">
+          </div>
+       
+        </div>
+</div>
+
+<!-- Modal ONE-->
+<div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl" style="padding-top:150px;">
+    <div class="modal-content" style="border-bottom-right-radius: calc(2rem);">
+      <div class="modal-header mods-head">
+       
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true" style="color:white;">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <ul style="list-style-type:none;padding: 0 0 0 20px; margin: 0;">
+          <li class="p-3">
+              <p class="mb-0" style="color: #435464; font-weight: 700;">*Percentage of patients stating goal is "Quite Important" or "Very important". <br> Online patient  survey of 500 adult participants with moderate to severe plaque psoriasis from United States.</p>
+          </li>
+        </ul>
+
+    
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- Modal Two-->
+<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl" style="padding-top:150px;">
+    <div class="modal-content" style="border-bottom-right-radius: calc(2rem);">
+      <div class="modal-header mods-head">
+       
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true" style="color:white;">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <ul style="list-style-type:none;padding: 0 0 0 20px; margin: 0;">
+          <li class="p-3">
+              <p class="mb-0" style="color: #435464; font-weight: 700;">*Percentage of patients stating goal is "Quite Important" or "Very important". <br> Online patient  survey of 500 adult participants with moderate to severe plaque psoriasis from United States.</p>
+          </li>      
+        </ul>
+
+        <div class="row">
+          <div class="col-sm-4" style="padding-left: 5rem;">
+              <div class="alert alert-primary" role="alert" style="border-radius: 1.3rem;background-color: #49AA41;">
+              <h1 style="padding-left: 4rem;font-weight: lighter;font-size: 4rem;color: white;">95%</h1>
+                  <h1 style="padding-left: .9rem;font-size: .8rem;color: white;margin-bottom:0;">of patients want to be  healed of</h1>
+                  <h1 style="padding-left: 2.8rem;color: white;margin-bottom:0;">all skin</h1>
+                  <h1 style="padding-left: 3rem;color: white;margin-bottom: 0;font-weight: 400;font-size: 2.2rem;">detects*</h1>
+              </div>
+           </div>
+         <div class="col-sm-8">
+          
+         <div class="card">
+            <div class="card-body">
+              This is some text within a card body.
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              This is some text within a card body.
+            </div>
+          </div>
+
+
+
+          </div>
+       </div>
+
+
+
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ layout/navbar.php
@@ -0,0 +1,73 @@
+
+<!-- for webview -->
+<?php echo'
+<div class="d-none  d-lg-block">
+<div class="container-fluid top-nav">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">LOGO</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav nav-menus">
+      <a class="nav-link" href="#">STUDY DESIGN</span></a>
+      <a class="nav-link" href="#"  data-toggle="modal" data-target="#exampleModal">REFERENCES</a>
+      <a class="nav-link" href="#">IMPORTANT SAFETY IMPORMATION</a>
+    </div>
+  </div>
+</nav>
+</div>
+</div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content" style="border-radius:2rem;">
+      <div class="modal-header mod-head">
+        <h5 class="modal-title mod-title" id="exampleModalLabel">REFERENCES</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <ul style="list-style-type:none;padding: 0 0 0 20px; margin: 0;">
+          <li class="p-3">
+              <p class="mb-0" style="color: #435464;">1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati enim reiciendis optio ex commodi consectetur molestias libero quae sed officiis amet voluptates quis itaque culpa necessitatibus, eius aspernatur, doloribus tempora.</p>
+          </li>
+          <li class="p-3">
+          <p class="mb-0" style="color: #435464;">2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati enim reiciendis optio ex commodi consectetur molestias libero quae sed officiis amet voluptates quis itaque culpa necessitatibus, eius aspernatur, doloribus tempora.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+';
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
